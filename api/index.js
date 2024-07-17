c2e8a52e3dd0a97150ac56a03f068d92917bd5e8
boby011 <abhishekbobby16@gmail.com>
2024-07-17 18:50:45 +0530

    updated

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT;
-require('dotenv').config
+
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
